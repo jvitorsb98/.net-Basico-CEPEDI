281099cac07b292105122a1ec95c9becca1f5642
João Ramos <jvitorsb98@outlook.com>
2023-11-14 08:36:29 -0300

    Ultimas correções na atividade 1

--- Semana1/Atividade-1/Atv-3/Program.cs
+++ Semana1/Atividade-1/Atv-3/Program.cs
@@ -1,5 +1,5 @@
 ﻿double valorDouble = 5.8;
-int valorInt = (int)valorDouble;
+int valorInt = (int)valorDouble; // Ao utilizar Convet irá ser aproximado o valor
 Console.WriteLine(valorInt);
 
 /*As casas decimais foram perdidas devido a forma de armazenamento
