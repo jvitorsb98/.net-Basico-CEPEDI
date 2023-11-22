aaea30e1c70d37c477424224a968b6c155106eda
João Ramos <jvitorsb98@outlook.com>
2023-11-22 12:12:04 -0300

    Aula 03

---
+++ Semana3/ExerciciosSala/exercicio5/Program.cs
@@ -0,0 +1,14 @@
+using exercicio5.Pessoa;
+
+
+public static void Main(string[] args){
+
+    string nome = console.ReadLine();
+    int idade = (int)console.ReadLine();
+    double altura = (double)console.ReadLine();
+
+    // Pessoa pessoa = new Pessoa(nome, idade, altura);
+    // pessoa.mostraPessoa();
+
+
+}
